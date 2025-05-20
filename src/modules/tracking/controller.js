6f2141acb2d7e28be7f1d7d14c3a3e3a21866b8e
Abel <pintorgarciaabel4142@gmail.com>
2025-05-20 13:50:30 -0600

    enhance error handling in order tracking and use the standard response handling

--- src/modules/tracking/controller.js
+++ src/modules/tracking/controller.js
@@ -1,5 +1,5 @@
-const db = require('../../DB/mysql')
-
+const db = require('../../DB/mysql.js')
+const answers = require('../../network/answers.js');
 
 async function orderTrackingController(req,res){
 
@@ -7,6 +7,10 @@ async function orderTrackingController(req,res){
     const allTransactions = await db.GetAll("transacciones");
     const transaction = allTransactions.find((  t )=> t.referencia == folio);
     
+    if (!transaction) {
+        return answers.error(req, res, 'No se encontró la transacción', 404);
+    }
+
     const response = {
         folio,
         estatus: transaction.estado,
@@ -14,7 +18,7 @@ async function orderTrackingController(req,res){
         hora: transaction.hora,
     }
 
-    res.status(200).json(response);
+    return answers.success(req, res, response, 200, 'Consulta exitosa', 200);
 
 }
 
